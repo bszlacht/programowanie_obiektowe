f9628f9e0ab5637f58723ff56e0080630f243764
bszlacht <73062284+bszlacht@users.noreply.github.com>
2020-11-01 14:47:43 +0100

    Add files via upload

--- laboratoria_zadania/src/test/lab1/AnimalTest.java
+++ laboratoria_zadania/src/test/lab1/AnimalTest.java
@@ -14,37 +14,31 @@ public class AnimalTest{
         cat.move(MoveDirection.RIGHT);
         cat.move(MoveDirection.FORWARD);
         cat.move(MoveDirection.FORWARD);
-        cat.move(MoveDirection.FORWARD);
-        Vector2d position = new Vector2d(4,2);
+        cat.move(MoveDirection.FORWARD);;
         assertEquals("pozycja: (4,2), orientacja: Wschód", cat.toString());
         // orientacja i pozycja się zgadza
-    }
 
-    @Test
-    public void moveTest2(){
-        Animal cat = new Animal();
-        cat.move(MoveDirection.RIGHT);
-        cat.move(MoveDirection.FORWARD);
-        cat.move(MoveDirection.FORWARD);
-        cat.move(MoveDirection.FORWARD);
-        cat.move(MoveDirection.FORWARD);
-        cat.move(MoveDirection.FORWARD);
-        cat.move(MoveDirection.FORWARD);
-        Vector2d position = new Vector2d(4,2);
-        assertEquals("pozycja: (4,2), orientacja: Wschód", cat.toString());
+        Animal cat2 = new Animal();
+        cat2.move(MoveDirection.RIGHT);
+        cat2.move(MoveDirection.FORWARD);
+        cat2.move(MoveDirection.FORWARD);
+        cat2.move(MoveDirection.FORWARD);
+        cat2.move(MoveDirection.FORWARD);
+        cat2.move(MoveDirection.FORWARD);
+        cat2.move(MoveDirection.FORWARD);
+
+        assertEquals("pozycja: (4,2), orientacja: Wschód", cat2.toString());
         // nie wychodzi poza mapę
-    }
 
-    @Test
-    public void moveTest3(){
-        Animal cat = new Animal();
-        cat.move(MoveDirection.LEFT);
-        cat.move(MoveDirection.BACKWARD);
-        cat.move(MoveDirection.LEFT);
+        Animal cat3 = new Animal();
+        cat3.move(MoveDirection.LEFT);
+        cat3.move(MoveDirection.BACKWARD);
+        cat3.move(MoveDirection.LEFT);
 
-        Vector2d position = new Vector2d(3,2);
-        assertEquals("pozycja: (3,2), orientacja: Południe", cat.toString());
+        assertEquals("pozycja: (3,2), orientacja: Południe", cat3.toString());
         // orientacja i pozycja się zgadza
     }
+
+
 
 }
--- laboratoria_zadania/src/test/lab1/OptionsParserTest.java
+++ laboratoria_zadania/src/test/lab1/OptionsParserTest.java
@@ -7,18 +7,15 @@ import static org.junit.Assert.*;
 
 public class OptionsParserTest {
     @Test
-    public void parseTest1(){
+    public void parseTest(){
         MoveDirection[] input = new MoveDirection[]{MoveDirection.RIGHT, MoveDirection.FORWARD, MoveDirection.FORWARD, MoveDirection.FORWARD};
         MoveDirection[] output = OptionsParser.parse(new String[]{"progra","right","mowanie","f","forward","obiektowe","f"});
         assertArrayEquals(input,output);
-    }
-    @Test
-    public void parseTest2(){
-        MoveDirection[] input = new MoveDirection[]{MoveDirection.LEFT, MoveDirection.BACKWARD, MoveDirection.LEFT, MoveDirection.FORWARD,MoveDirection.FORWARD,MoveDirection.FORWARD,MoveDirection.FORWARD,MoveDirection.FORWARD};
-        MoveDirection[] output = OptionsParser.parse(new String[]{"progra","l","mowanie","b","left","obiektowe","f","f","f","f","f"});
-        assertArrayEquals(input,output);
-    }
 
+        MoveDirection[] input2 = new MoveDirection[]{MoveDirection.LEFT, MoveDirection.BACKWARD, MoveDirection.LEFT, MoveDirection.FORWARD,MoveDirection.FORWARD,MoveDirection.FORWARD,MoveDirection.FORWARD,MoveDirection.FORWARD};
+        MoveDirection[] output2 = OptionsParser.parse(new String[]{"progra","l","mowanie","b","left","obiektowe","f","f","f","f","f"});
+        assertArrayEquals(input2,output2);
+    }
 
     //dane wejściowe podane jako tablica łańcuchów znaków są poprawnie interpretowane
 
