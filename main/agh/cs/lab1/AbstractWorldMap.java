3b9baefc69042511c33081df7f6811256f10f057
Zbigniew "Radagast" Kaleta <zkaleta@agh.edu.pl>
2020-12-03 13:57:11 +0100

    Komentarz do lab 7

--- main/agh/cs/lab1/AbstractWorldMap.java
+++ main/agh/cs/lab1/AbstractWorldMap.java
@@ -1,29 +1,30 @@
 package agh.cs.lab1;
 
 import java.util.HashMap;
-public abstract class AbstractWorldMap implements IWorldMap,IPositionChangeObserver{
+
+public abstract class AbstractWorldMap implements IWorldMap, IPositionChangeObserver {
+    protected final HashMap<Vector2d, Animal> animalHashMap = new HashMap<>();
+    protected final MapBoundary boundary = new MapBoundary();   // po co to jest tutaj, skoro RectangularMap tego nie używa?
     private final MapVisualizer map = new MapVisualizer(this);
-    protected final  HashMap<Vector2d, Animal> animalHashMap = new HashMap<>();
-    protected final MapBoundary boundary = new MapBoundary();
 
     public boolean isOccupied(Vector2d position) {
         return objectAt(position) != null;
     }
 
-    public boolean place(Animal animal) throws IllegalArgumentException{
-        if(this.canMoveTo(animal.getPosition())){
+    public boolean place(Animal animal) throws IllegalArgumentException {
+        if (this.canMoveTo(animal.getPosition())) {
             this.animalHashMap.put(animal.getPosition(), animal);
             animal.addObserver(this);
             this.boundary.place(animal.getPosition());
-            animal.addObserver(this.boundary);
+            animal.addObserver(this.boundary);  // czemu MapBoundary nie dba samo o siebie?
             return true;
-        }else{
+        } else {
             throw new IllegalArgumentException("Animal cannot be placed at position " + animal.getPosition().toString());
         }
 
     }
 
-    public boolean canMoveTo(Vector2d position){
+    public boolean canMoveTo(Vector2d position) {
         return !(objectAt(position) instanceof Animal);
     }
 
@@ -32,17 +33,18 @@ public abstract class AbstractWorldMap implements IWorldMap,IPositionChangeObser
     }
 
     protected abstract Vector2d getLowerLeft();
+
     protected abstract Vector2d getUpperRight();
 
-    public String toString(){
-        return map.draw(getLowerLeft(),getUpperRight());
+    public String toString() {
+        return map.draw(getLowerLeft(), getUpperRight());
     }
 
     @Override
-    public void positionChanged(Vector2d oldPosition, Vector2d newPosition){
-        Animal element = (Animal)this.objectAt(oldPosition);
+    public void positionChanged(Vector2d oldPosition, Vector2d newPosition) {
+        Animal element = (Animal) this.objectAt(oldPosition);   // zwierzę już powinno być na nowej pozycji
         animalHashMap.remove(oldPosition);
-        animalHashMap.put(newPosition,element);
-        boundary.positionChanged(oldPosition,newPosition);
+        animalHashMap.put(newPosition, element);
+        boundary.positionChanged(oldPosition, newPosition); // zwierzę ma notyfikować obserwatorów, a nie oni siebie nawzajem
     }
 }
